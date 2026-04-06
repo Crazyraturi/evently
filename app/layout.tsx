e0a1f76f281b50ebc8665ac7ee7b8a006d11366f
SUBODH RATURI <96306193+Crazyraturi@users.noreply.github.com>
2026-04-06 22:06:20 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import {Schibsted_Grotesk, Martian_Mono,} from "next/font/google";
+import "./globals.css";
+import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "The Hub for Every Dev Event You Mustn't Miss.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${martianMono.variable} ${schibstedGrotesk.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+      <div className="absolute inset-0 top-0  z-[-1] min-h-screen">
+        <Navbar/>
+        <LightRays
+            raysOrigin="top-center"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0}
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+        />
+      </div>
+      <main>
+        {children}
+      </main>
+      </body>
+    </html>
+  );
+}
